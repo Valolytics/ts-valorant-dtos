a5ddf5a169bcaf85ca8acc1cd0856e9eb845d8e0
monkaCode <f.h612@hotmail.com>
2025-04-06 00:37:54 +0200

    added static agent get properties on AGENTS

--- src/services/dtos.service.ts
+++ src/services/dtos.service.ts
@@ -70,6 +70,34 @@ export class Agents extends Collection<Agent> {
     constructor(json: any) {
         super(json, Agent);
     }
+
+    get astra(): Agent { return this.items["41fb69c1-4189-7b37-f117-bcaf1e96f1bf"]; }
+    get breach(): Agent { return this.items["5f8d3a7f-467b-97f3-062c-13acf203c006"]; }
+    get brimstone(): Agent { return this.items["9f0d8ba9-4140-b941-57d3-a7ad57c6b417"]; }
+    get chamber(): Agent { return this.items["22697a3d-45bf-8dd7-4fec-84a9e28c69d7"]; }
+    get clove(): Agent { return this.items["1dbf2edd-4729-0984-3115-daa5eed44993"]; }
+    get cypher(): Agent { return this.items["117ed9e3-49f3-6512-3ccf-0cada7e3823b"]; }
+    get deadlock(): Agent { return this.items["cc8b64c8-4b25-4ff9-6e7f-37b4da43d235"]; }
+    get fade(): Agent { return this.items["dade69b4-4f5a-8528-247b-219e5a1facd6"]; }
+    get gekko(): Agent { return this.items["e370fa57-4757-3604-3648-499e1f642d3f"]; }
+    get harbor(): Agent { return this.items["95b78ed7-4637-86d9-7e41-71ba8c293152"]; }
+    get iso(): Agent { return this.items["0e38b510-41a8-5780-5e8f-568b2a4f2d6c"]; }
+    get jett(): Agent { return this.items["add6443a-41bd-e414-f6ad-e58d267f4e95"]; }
+    get kayo(): Agent { return this.items["601dbbe7-43ce-be57-2a40-4abd24953621"]; }
+    get killjoy(): Agent { return this.items["1e58de9c-4950-5125-93e9-a0aee9f98746"]; }
+    get neon(): Agent { return this.items["bb2a4828-46eb-8cd1-e765-15848195d751"]; }
+    get omen(): Agent { return this.items["8e253930-4c05-31dd-1b6c-968525494517"]; }
+    get phoenix(): Agent { return this.items["eb93336a-449b-9c1b-0a54-a891f7921d69"]; }
+    get raze(): Agent { return this.items["f94c3b30-42be-e959-889c-5aa313dba261"]; }
+    get reyna(): Agent { return this.items["a3bfb853-43b2-7238-a4f1-ad90e9e46bcc"]; }
+    get sage(): Agent { return this.items["569fdd95-4d10-43ab-ca70-79becc718b46"]; }
+    get skye(): Agent { return this.items["6f2a04ca-43e0-be17-7f36-b3908627744d"]; }
+    get sova(): Agent { return this.items["320b2a48-4d9b-a075-30f1-1f93a9b638fa"]; }
+    get tejo(): Agent { return this.items["b444168c-4e35-8076-db47-ef9bf368f384"]; }
+    get viper(): Agent { return this.items["707eab51-4836-f488-046a-cda6bf494859"]; }
+    get vyse(): Agent { return this.items["efba5359-4016-a1e5-7626-b1ae76895940"]; }
+    get waylay(): Agent { return this.items["df1cb487-4902-002e-5c17-d28e83e78588"]; }
+    get yoru(): Agent { return this.items["7f94d92c-4234-0a36-9646-3a87eb8b5c89"]; }
 }
 export class Buddies extends Collection<Buddy> {
     constructor(json: any) {
